674ec99fcf541815d8a324ebc46f0f131e54ede8
Yan <yangalperin@gmail.com>
2022-09-05 17:03:00 +0300

    Task1

---
+++ Task_1/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по возрастанию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 1 2 4 7
+// 2 3 5 9
+// 2 4 4 8
+
+Console.WriteLine("Введите число строк массива m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число столбцов массива n:");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Random rand = new Random();
+            matr[i, j] = rand.Next(1, 10);
+        }
+    }
+}
+void SwapArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int h = 0; h < matr.GetLength(1) - 1; h++)
+            {
+                if (matr[i, h] > matr[i, h + 1])
+                {
+                    int temp = matr[i, h];
+                    matr[i, h] = matr[i, h + 1];
+                    matr[i, h + 1] = temp;
+                }
+            }
+        }
+    }
+}
+FillArray(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+SwapArray(matrix);
+PrintArray(matrix);
+
