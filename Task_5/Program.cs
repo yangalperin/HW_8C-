5c09a8749bcf244804ab1ef104db13b439fb5e2d
Yan <yangalperin@gmail.com>
2022-09-05 17:14:19 +0300

    Task5

---
+++ Task_5/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 62: Заполните спирально массив 4 на 4.
+// 1 2 3 4
+// 12 13 14 5
+// 11 16 15 6
+// 10 9 8 7
+
+
+int n = 4;
+int[,] matrix = new int[n, n];
+
+
+int i = 0;
+int j = 0;
+int temp = 1;
+while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+{
+    matrix[i, j] = temp;
+    temp++;
+
+    
+    
+    if (i <= j + 1 && i + j < n - 1)
+        j++;
+    else if (i < j && i + j >= n - 1)
+        i++;
+    else if (i >= j && i + j > n - 1)
+        j--;
+    else
+        i--;
+
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+PrintArray(matrix);
+Console.WriteLine();
+
